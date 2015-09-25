3a2575fe32641b7fd158412918e66f952ec0d0c3
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-09-25 21:27:49 +0200

    fix jscs errors in months.js

--- months.js
+++ months.js
@@ -21,81 +21,85 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 window.charts.months = function ( data, namespaces, colors ) {
+	var n, m, margin, width, height, stack, layers,
+		yGroupMax, yStackMax, x, y, xAxis, yAxis, yAxis2,
+		tip, svg, layer;
 
-	var n = namespaces.length,
-		m = data.length,
+	n = namespaces.length;
+	m = data.length;
 
-		margin = {
-			top: 40,
-			right: 200,
-			bottom: 30,
-			left: 90
-		},
-		width = document.body.clientWidth - 80 - margin.left - margin.right,
-		height = ( 700 / 28 * m ) - margin.top - margin.bottom,
+	margin = {
+		top: 40,
+		right: 200,
+		bottom: 30,
+		left: 90
+	};
+	width = document.body.clientWidth - 80 - margin.left - margin.right;
+	height = ( 700 / 28 * m ) - margin.top - margin.bottom;
 
-		stack = d3.layout.stack(),
-		layers = stack( d3.range( n ).map( function ( d ) {
-			var a = [];
-			for ( var i = 0; i < m; ++i ) {
-				a[i] = {
-					ns: namespaces[d],
-					x: i,
-					y: data[i][1][d]
-				};
-			}
-			return a;
-		} ) ),
+	stack = d3.layout.stack();
+	layers = stack( d3.range( n ).map( function ( d ) {
+		var i,
+			a = [];
+		for ( i = 0; i < m; ++i ) {
+			a[i] = {
+				ns: namespaces[d],
+				x: i,
+				y: data[i][1][d]
+			};
+		}
+		return a;
+	} ) );
 
-		yGroupMax = d3.max( layers, function ( layer ) {
-				return d3.max( layer, function ( d ) {
-					return d.y;
-				} );
-			} ),
-		yStackMax = d3.max( layers, function ( layer ) {
-				return d3.max( layer, function ( d ) {
-					return d.y0 + d.y;
-				} );
-			} ),
+	yGroupMax = d3.max( layers, function ( layer ) {
+			return d3.max( layer, function ( d ) {
+				return d.y;
+			} );
+		} );
+	yStackMax = d3.max( layers, function ( layer ) {
+			return d3.max( layer, function ( d ) {
+				return d.y0 + d.y;
+			} );
+		} );
 
-		x = d3.scale.linear()
-			.domain( [ 0, yStackMax ] )
-			.range( [ 0, width ] ),
+	x = d3.scale.linear()
+		.domain( [ 0, yStackMax ] )
+		.range( [ 0, width ] );
 
-		y = d3.scale.ordinal()
-			.domain( d3.range( m ) )
-			.rangeRoundBands( [ 0, height ], 0.2 ),
+	y = d3.scale.ordinal()
+		.domain( d3.range( m ) )
+		.rangeRoundBands( [ 0, height ], 0.2 );
 
-		xAxis = d3.svg.axis()
-			.scale( x )
-			.orient( 'bottom' ),
+	xAxis = d3.svg.axis()
+		.scale( x )
+		.orient( 'bottom' );
 
-		// year and month number
-		yAxis = d3.svg.axis()
-			.scale( y )
-			.orient( 'left' )
-			.tickFormat( function ( d, i ) { return data[i][0]; } ),
+	// year and month number
+	yAxis = d3.svg.axis()
+		.scale( y )
+		.orient( 'left' )
+		.tickFormat( function ( d, i ) { return data[i][0]; } );
 
-		// total month count
-		yAxis2 = d3.svg.axis()
-			.scale( y )
-			.orient( 'left' )
-			.tickFormat( function ( d, i ) { return d3.sum( data[i][1] ); } ),
+	// total month count
+	yAxis2 = d3.svg.axis()
+		.scale( y )
+		.orient( 'left' )
+		.tickFormat( function ( d, i ) { return d3.sum( data[i][1] ); } );
 
-		tip = d3.tip()
-			.attr( 'class', 'svg-tip svg-tip-small' )
-			.direction( 'e' )
-			.offset( [ 0, 6 ] )
-			.html( function ( d, i ) {
-				return d.ns + ': ' + d.y;
-			} ),
+	tip = d3.tip()
+		.attr( 'class', 'svg-tip svg-tip-small' )
+		.direction( 'e' )
+		.offset( [ 0, 6 ] )
+		.html( function ( d, i ) {
+			return d.ns + ': ' + d.y;
+		} );
 
-		svg = d3.select( '#month-chart' )
-				.append( 'svg' )
-				.attr( 'width', width + margin.left + margin.right )
-				.attr( 'height', height + margin.top + margin.bottom )
-			.append( 'g' )
-				.attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' );
+	svg = d3.select( '#month-chart' )
+			.append( 'svg' )
+			.attr( 'width', width + margin.left + margin.right )
+			.attr( 'height', height + margin.top + margin.bottom )
+		.append( 'g' )
+			.attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' );
 
 	svg.call( tip );
 
@@ -114,7 +118,7 @@ window.charts.months = function ( data, namespaces, colors ) {
 			.attr( 'class', 'y axis' )
 			.call( yAxis2 );
 
-	var layer = svg.selectAll( '.layer' )
+	layer = svg.selectAll( '.layer' )
 		.data( layers )
 	.enter().append( 'g' )
 		.attr( 'class', 'layer' )
